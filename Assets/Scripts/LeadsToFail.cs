a5551f3c4cf76d351a616c8dc9a734fa249c1c5c
Denis Kerzhemanov <kerjemanov@yandex.ru>
2020-01-11 03:45:43 +0300

    initial

---
+++ Assets/Scripts/LeadsToFail.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+
+[assembly: RegisterGenericComponentType(typeof(Generic<SomeStruct>))]
+
+public struct Generic<T> : IBufferElementData
+        where T : struct
+{
+}
+
+public class ChunkGeneratorSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        Entities.ForEach((in DynamicBuffer<Generic<int>> nodes) => {}).Run();
+        return inputDependencies;
+    }
+}
